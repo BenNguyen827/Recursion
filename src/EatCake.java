f29010a3065facaae6cf6a429b49de22a67ca25c
Benjamin <{bennguyening827@gmail.com}>
2020-11-09 16:24:43 -0600

    Initial Commit

---
+++ src/EatCake.java
@@ -0,0 +1,14 @@
+
+public class EatCake {
+public EatCake () {
+	
+}
+public void eatCake(Cake cake) {
+	eatCake(cake);
+}
+public static void main(String[] args) {
+	Cake cake  = new Cake();
+	eatCake(cake);
+	
+}
+}
