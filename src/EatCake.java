87377e651a890331f8026c854b738be367c4ef10
Benjamin <{bennguyening827@gmail.com}>
2020-11-09 16:31:30 -0600

    cake and EatCake

--- src/EatCake.java
+++ src/EatCake.java
@@ -4,11 +4,15 @@ public EatCake () {
 	
 }
 public void eatCake(Cake cake) {
+	if(!cake.isEmpty()) {
+	cake.takeABite();
 	eatCake(cake);
+	}
 }
 public static void main(String[] args) {
 	Cake cake  = new Cake();
-	eatCake(cake);
+	eatCake1(cake);
+	System.out.println("Done eating cake " + " - ready to go back to Java 2!!!");
 	
 }
 }
